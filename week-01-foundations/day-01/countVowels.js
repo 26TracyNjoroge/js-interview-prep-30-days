12c7277fc32193de7a1453fafd2267c1fface062
Tracy Njoroge <tracywangari997@gmail.com>
2025-12-03 01:32:39 +0300

    Update countVowels function

--- week-01-foundations/day-01/countVowels.js
+++ week-01-foundations/day-01/countVowels.js
@@ -1,1 +1,16 @@
-const str = "Tracy";
+const countVowels = (str) => {
+    const vowels = "aeiou";
+    let count = 0;
+
+    for(let i = 0; i < str.length; i++) {
+        if(vowels.includes(str[i])) {
+            count++;
+        } 
+    }
+    
+    return count;
+}
+
+console.log(countVowels("hello"));
+console.log(countVowels("Tracy"));
+
