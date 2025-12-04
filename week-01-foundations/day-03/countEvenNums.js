46c6e764dba44062edbffc859bcbe33e3e9d50c1
Tracy Njoroge <tracywangari997@gmail.com>
2025-12-04 23:47:50 +0300

    Update countEvenNums function

--- week-01-foundations/day-03/countEvenNums.js
+++ week-01-foundations/day-03/countEvenNums.js
@@ -1,10 +1,21 @@
 const countEvenNums = (arry) => {
     if (!Array.isArray(arry) || arry.length === 0) return undefined;
+
+    let count = 0;
+
+    for (let i=0;i<arry.length;i++) {
+        if (arry[i] % 2 === 0) {
+            count++;
+        }
+    }
+
+    return count;
 }
 
 const myArray = [2, 45, 34, 25, 85, 12];
 const myArray2 = "Trial";
 const myArray3 = [];
 
-console.log(countEvenNums(myArray2));
-console.log(countEvenNums(myArray3));
+console.log(countEvenNums(myArray));   //3
+console.log(countEvenNums(myArray2));  //undefined
+console.log(countEvenNums(myArray3));  //undefined
