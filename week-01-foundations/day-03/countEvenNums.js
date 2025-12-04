7981090901ba445c8fd470d5c23852326cc9eaa8
Tracy Njoroge <tracywangari997@gmail.com>
2025-12-04 23:43:01 +0300

    Update day 3 challenges

---
+++ week-01-foundations/day-03/countEvenNums.js
@@ -0,0 +1,10 @@
+const countEvenNums = (arry) => {
+    if (!Array.isArray(arry) || arry.length === 0) return undefined;
+}
+
+const myArray = [2, 45, 34, 25, 85, 12];
+const myArray2 = "Trial";
+const myArray3 = [];
+
+console.log(countEvenNums(myArray2));
+console.log(countEvenNums(myArray3));
